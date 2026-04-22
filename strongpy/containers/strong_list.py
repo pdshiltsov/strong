e1ba87847c5dc840717381433231c3aec6063f99
Pavel Shiltsov <pd.shiltsov@gmail.com>
2026-04-22 17:45:43 +0200

    Added StrongDict

--- strongpy/containers/strong_list.py
+++ strongpy/containers/strong_list.py
@@ -1,28 +1,32 @@
-from strongpy.type_checker import _type_checker
 from copy import deepcopy
+
+from strongpy.type_checker import _type_checker
 
 
 class StrongList:
-    def __init__(self, annotation, obj):
+    ''' def __init__(self, annotation, obj):
         self.annotation = annotation
         self.__data = []
 
-        for item in obj:
+        for item in deepcopy(obj):
             if _type_checker(obj, annotation):
                 self.__data.append(obj)
             else:
-                raise TypeError(f"{self.__name__} only accepts {self.annotation}, not {type(item)}") 
-
+                raise TypeError(f"{self.__name__}"
+                    f"only accepts {self.annotation}, not {type(item)}") 
+    '''
     def __class_getitem__(cls, annotation):
         class TypedStrongList(cls):
             def __init__(self, obj):
                 self.__data = []
                 
-                for item in obj:
+                for item in deepcopy(obj):
                     if _type_checker(obj, annotation):
                         self.__data.append(obj)
                     else:
-                        raise TypeError(f"{self.__name__} only accepts {self.annotation}, not {type(item)}") 
+                        raise TypeError(f"{self.__name__}"
+                        f"only accepts {self.annotation}, not {type(item)}") 
+
 
             def __getitem__(self, key):
                 if hasattr(self.__data[key], "__hash__"):
